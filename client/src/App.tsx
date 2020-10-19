334f6e61fc4e6ec826ce1d54f1d84e305402912c
ejaz khan <khaner@gmatc.matc.edu>
2020-10-19 13:15:20 -0500

    Completed Activity 4

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+class App extends React.Component{
+state = {
+  data:null
+}
+componentDidMount(){
+axios.get('http://localhost:5000')
+.then((response) => {
+this.setState({
+  data: response.data
+})
+})
+.catch((error) => {
+console.error('Error fetching data: ${error}');
 
-function App() {
+})
+}
+
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      GoodThings
       </header>
+      {this.state.data}
     </div>
   );
 }
 
-export default App;
+}
